2ef0e305373b02809b6530b88e4ccfd3283378fb
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-04-23 20:22:04 +0500

    Intitialize databse before tests

--- tests/node_api.test.js
+++ tests/node_api.test.js
@@ -1,10 +1,31 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert")
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+
+const blogs = [
+  {
+    "_id": "5a422aa71b54a676234d17f8", "title": "Go To Statement Considered Harmful", "author": "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", "likes": 5, "__v": 0
+  },
+  {
+    "_id": "5a422b3a1b54a676234d17f9", "title": "Canonical string reduction", "author": "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", "likes": 12, "__v": 0
+  }
+]
+
+// Delete previous blogs and add new ones
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(blogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(blogs[1]);
+  await blogObject.save();
+});
+
 
 test("blogs are returned as json", async () => {
   await api
@@ -13,15 +34,15 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 })
 
-test("there are six blogs", async () => {
+test("there are two blogs", async () => {
   const response = await api.get("/api/blogs");
-  assert.strictEqual(response.body.length, 6);
+  assert.strictEqual(response.body.length, blogs.length);
 })
 
-test("the first blog is about React patterns", async () => {
+test("the first blog is about Statement Harmful", async () => {
   const response = await api.get("/api/blogs");
   const contents = response.body.map(blog => blog.title);
-  assert(contents.includes("React patterns"));
+  assert(contents.includes("Go To Statement Considered Harmful"));
 })
 
 after(() => {
